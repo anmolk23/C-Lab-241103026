a3bc2281520955ac17b2e343436f645238114689
Anmol Kumar <anmolkumarofficial6372@gmail.com>
2025-08-19 20:28:34 +0530

    Create S1P2.c

---
+++ S1P2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void incr1( int *a)
+{
+    *a=*a+1;
+    
+}
+int main()
+{
+    int a;
+    printf("Enter a number");
+    scanf("%d", &a);
+    incr1(&a);
+    printf("The value of a after incr1 is: %d", a);
+}
