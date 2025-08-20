3298cb0be661252fa8a557f33b7ae7bcc928b062
Anmol Kumar <anmolkumarofficial6372@gmail.com>
2025-08-21 03:47:17 +0530

    Create S1P8.c

---
+++ S1P8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int strLength(char *str)
+{
+    int str_length = 0;
+while (*str != '\0')
+    {
+        str_length++;
+        str += 1;
+    }
+    return str_length;
+}
+int main()
+{
+    char *str = "Web Tech";
+    int str_length = strLength(str);
+    printf("Length of the string is %d", str_length);
+    return 0;
+}
